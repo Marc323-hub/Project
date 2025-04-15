d013015b2ee6c02235fdc48d87a88ad7c13df2e9
Marc323-hub <marc.stoecklin@students.fhnw.ch>
2025-04-15 17:17:36 +0200

    Update guest_manager.py
    Code von Deepnote importiert

---
+++ business_logic/guest_manager.py
@@ -0,0 +1,27 @@
+Class Guests:
+    def __init__(self, name, email, phone_number, address, guest-id):
+        self.__name = name
+        self.__email = email
+        self.__phone_number = phone_number
+        self.__address = address
+        self.__guest-id = []]
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def email(self):
+        return self.__email
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
+    @property
+    def address(self):
+        return self.__address
+
+    @property
+    def guest-id(self):
+        return self.__guest-id
